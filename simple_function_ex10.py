c782b040c0797c5b3bcae5d5ca73a9c02ff93729
K S Sreenivasa Raghavan <sreenivas.raghavan7@gmail.com>
2022-11-05 16:09:50 +0530

    Revert "Added new n*2 as per choice"
    This reverts commit 1e7346ac02ebd83868185ddc2e140a6364d0eb28.

--- simple_function_ex10.py
+++ simple_function_ex10.py
@@ -2,7 +2,7 @@
 # to call the function again and again with the range of values
 
 def count_to_n(n):
-    for i in range(1, n*2):
+    for i in range(1, n + 1):
         print(i, end=' ')
     print()
 
